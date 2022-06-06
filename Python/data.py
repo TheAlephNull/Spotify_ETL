3b48b167c1774e3092ba7390e6f13e53e249193b
henrya1 <h.aguilar3.1415@gmail.com>
2022-06-06 14:50:40 -0700

    finished preparing the API

--- Python/data.py
+++ Python/data.py
@@ -1,5 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 
 import csv
 import boto3
@@ -8,3 +10,17 @@ import sys
 
 from datetime import datetime
 
+def configure():
+    load_dotenv()
+
+def setup_etl():
+    """call api key using os.get('api_key')"""
+    spotify_client_id = os.get('spotify_client_id')
+    spotify_client_secret = os.get('spotify_client_secret')
+    spotify_redirect_url = "http://localhost"
+    scope = "user-read-recently-played"
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = sp.current_user_recently_played(int = 15)
+    print(results)
