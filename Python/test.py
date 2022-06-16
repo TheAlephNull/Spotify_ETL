fbaadf3568141e41758358fa75b6e86a61a79348
henrya1 <h.aguilar3.1415@gmail.com>
2022-06-16 13:56:16 -0700

    finished most of transformation

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,7 @@
 import extract
 import transform
 import pandas as pd
+from datetime import datetime, timezone
 
 extract.configure()
 results = extract.extract_spotify()
@@ -8,14 +9,28 @@ results = extract.extract_spotify()
 # print(extract.artists(results))
 # print(extract.tracks(results)) 
 
-# Test whether the dataframes were created successfully
+## Test whether the dataframes were created successfully
 albumsdf = transform.AlbumsDataFrame(extract.albums(results))
 artistsdf = transform.ArtistsDataFrame(extract.artists(results))
 tracksdf = transform.TracksDataFrame(extract.tracks(results))
-#print(albumsdf['AlbumReleaseDate'])
-print(tracksdf['TrackTimePlayed'])
-print(pd.to_datetime(tracksdf['TrackTimePlayed'])).tz_convert('US/Pacific')
+# print(albumsdf['AlbumReleaseDate'])
 
-albumsdf.info()
-artistsdf.info()
-tracksdf.info()
+## testing datetime on TrackTimePlayed
+#print(tracksdf['TrackTimePlayed'])
+#tracksdf['TrackTimePlayed'] = pd.to_datetime(tracksdf['TrackTimePlayed'])
+#print(tracksdf['TrackTimePlayed'])
+# print(tracksdf['TrackTimePlayed'].dt.tz_convert('US/Pacific'))
+#tracksdf['TrackTimePlayed'] = tracksdf['TrackTimePlayed'].dt.tz_convert('US/Pacific')
+# ATTEMPT 1 ------------------------------------------------------------------
+# tracksdf['TrackTimePlayed'] = tracksdf['TrackTimePlayed'].astype(str).str[:-7]
+# print(tracksdf['TrackTimePlayed'])
+# tracksdf['TrackTimePlayed'] = pd.to_datetime(tracksdf['TrackTimePlayed'])
+# print(tracksdf['TrackTimePlayed']),
+# ATTEMPT 2 ------------------------------------------------------------------
+#tracksdf['TrackTimePlayed'] = tracksdf['TrackTimePlayed'].dt.tz_convert(None)
+#print(tracksdf['TrackTimePlayed'])
+
+## testing dtypes of each dataframe
+# albumsdf.info()
+# artistsdf.info()
+# tracksdf.info()
--- Python/transform.py
+++ Python/transform.py
@@ -3,7 +3,7 @@ import boto3
 import pandas as pd
 import sys
  
-from datetime import datetime
+from datetime import datetime, timezone
 
 import extract
 
@@ -71,12 +71,33 @@ def ArtistsDataFrame(artists):
     clean(artists_df)
     return artists_df
 
+def transform_date(df, toDate, zone='US/Pacific'):
+    """
+    DOCSTRING: (HELPER FUNCTION) Transforms tracks_df[toDate] to 
+    approrpiate DateTime object without the timezone stamp for 
+    SQL compatibility
+    INPUT: df (Pandas DataFrame) - of data structure, 
+    toDate (list) - list of columns that need to be fixed
+    timezone (string) - preferred time zone for conversion before UNIX
+    RETURNS: tracks_df[toDate]
+    """
+    df_copy = df.copy()
+    # for column in list provided: change to datetime
+    df_copy[toDate] = df_copy[toDate].apply(pd.to_datetime)
+    # change to local time zone by indexing first element of toDate 
+    # (since only one element)
+    df_copy[toDate[0]] = df_copy[toDate[0]].dt.tz_convert(zone)
+    # remove time zone portion form DateTime object (not supported in SQL)
+    df_copy['TrackTimePlayed'] = df_copy['TrackTimePlayed'].dt.tz_convert(None)
+
+    return df_copy[toDate]
+
 def transform_albums(albums_df):
     """
     DOCSTRING: Takes dataframe and transforms necessary 
     columns to preferred format
     INPUT: df (Pandas DataFrame) 
-    RESULTS: albums_df (Pandas DataFrame)
+    RETURNS: albums_df (Pandas DataFrame)
     """
     toString = ['AlbumID','AlbumName','AlbumURL','AlbumType']
     toDate = ['AlbumReleaseDate']
@@ -84,9 +105,28 @@ def transform_albums(albums_df):
     # change specified columns to string
     albums_df[toString] = albums_df[toString].astype(str)
     # for column in list provided: change to datetime
-    albums_df[toDate] = albums_df[toDate].apply(pd.to_datetime)
+    albums_df[toDate] = transform_date(albums_df, toDate, 'US/Pacific')
 
     return albums_df
+
+def create_uniqueID(tracks_df):
+    """
+    DOCSTRING: (HELPER FUNCTION) Creates a unique identifier for each song 
+    in the track DataFrame to ensure that no one song is played twice 
+    at the same time. Combines UNIX timestamp of TrackTimePlayed 
+    (from datetime) plus SpotifyTrackID
+    INPUTS: tracks_df (Pandas DataFrame) - of all song lists extracted
+    RETURNS: tracks_df['UniqueID']
+    """
+    # convert column to unix 
+    tracks_copy = tracks_df.copy()
+    tracks_copy['UNIXTimestamp'] = tracks_copy['TrackTimePlayed'].dt.timestamp()
+    tracks_copy['UNIXTimestamp'] = tracks_copy['UNIX Timestamp'].astype(str)
+
+    # create Unique ID
+    tracks_copy['UniqueID'] = tracks_copy['SpotifyTrackID'] + "-" + tracks_copy['UNIXTimestamp']
+
+    return tracks_copy['UniqueID']
 
 def transform_tracks(tracks_df):
     """
@@ -94,16 +134,43 @@ def transform_tracks(tracks_df):
     columns to preferred format. Also, original extracted
     date format is: "2022-06-15T20:41:47.672Z"
     INPUT: df (Pandas DataFrame) 
-    RESULTS: albums_df (Pandas DataFrame)
+    RETURNS: albums_df (Pandas DataFrame)
     """
     toString = ['SpotifyTrackID','TrackName','TrackURL','ArtistID','AlbumID']
     toDate = ['TrackTimePlayed']
 
     # change specified columns to string
     tracks_df[toString] = tracks_df[toString].astype(str)
-    # for column in list provided: change to datetime
-    tracks_df[toDate] = tracks_df[toDate].apply(pd.to_datetime)
+    # change date columns to dates without timezones
+    tracks_df[toDate] = transform_date(tracks_df, toDate, 'US/Pacific')
+    # create unique identifier based on song number and time played
+    tracks_df['UniqueID'] = create_uniqueID(tracks_df)
+    # make UniqueID as first column
+    UniqueID = tracks_df.pop('UniqueID')
+    tracks_df.insert(0, 'UniqueID', UniqueID)
 
     return tracks_df
+
+def transform_artists(artists_df):
+    """
+    DOCSTRING: Takes dataframe and makes necessary 
+    changes to the data. In this case, converting data type
+    to strings
+    INPUTS: artists_df (Pandas DataFrame) - from spotify extraction
+    RETURNS: artists_df (Pandas DataFrame)
+    """
+    toString = ['ArtistID','ArtistName','ArtistURL']
+    artists_df[toString] = artists_df[toString].astype(str)
+
+    return artists_df
+
+def transform(tracks_df, albums_df, artists_df):
+    """
+    DOCSTRING:
+    INPUTS:
+    RETURNS:
+    """
+    pass
+
 # Notes:
 # error with numpy and mkl-service solved using https://github.com/ThilinaRajapakse/simpletransformers/issues/322
